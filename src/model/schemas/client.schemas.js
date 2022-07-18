8a2c08eca0b0db79b7c52a46294e715c4f114251
Japplet <juantriana@seti.com.co>
2022-07-18 15:31:18 -0500

    :tada: Creamos el Esquema del Client

---
+++ src/model/schemas/client.schemas.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const ClientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+    },
+    document_id: {
+        type: number,
+    },
+    number_phone: {
+        type: number
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    neighborhood: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: Number,
+    },
+    installation_date: {
+        type: Date,
+    },
+    payday: {
+        type: Number,
+        max: 31,
+        required: true,
+    },
+    monthly_value: {
+        type: Number,
+        default: 40000,
+    },
+    status: {
+        type: String,
+        default: 'Activo',
+    }
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+});
+
+const Client = mongoose.model('Client', ClientSchema);
+
+module.exports = Client
